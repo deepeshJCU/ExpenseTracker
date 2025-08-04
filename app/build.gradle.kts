c5126555ed60a351b933216d17920a5bfe0e0df6
deepeshJCU <deepesh.bijarnia@my.jcu.edu.au>
2025-08-04 10:18:04 +0100

    Update the App gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,12 +69,27 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+
+    // Compose and Material3
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.material:material-icons-extended:1.6.1")
+
+    //Theme mode
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
     // Charts
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.1.0")
+
+//    work manager
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
 
     // Testing
     testImplementation(libs.junit)
